780b3b87d6e967fd17eacf27501c4123b524da1f
Ticomware <cbialo2@outlook.com>
2022-11-19 18:32:51 -0700

    Built getQuakesByLocation

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ import { getJSON, getLocation } from "./utilities.js";
 const baseUrl =
   "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
 
-const getQuakesByLocation = () => {
-  let location = getLocation();
-  //Format locationData in the form of URL params, like so:
-  //let locationData = &latitude=${latitudeValue}&longitude=${longitudeValue}&maxradiuskm=100
-  //let quakes = getJSON(baseUrl+locationData);
+async function getQuakesByLocation() {
+  getLocation().then((result) => {
+    let latitudeValue = result.coords.latitude;
+    let longitudeValue = result.coords.longitude;
+    let locationData = `&latitude=${latitudeValue}&longitude=${longitudeValue}&maxradiuskm=100`;
+    let quakes = getJSON(baseUrl+locationData);
+    console.log(quakes);
+  });
 }
+
+getQuakesByLocation();
